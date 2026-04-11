c9627cd9b5c05992913e52227bea028b0e120382
Ouma Emmanuel Odhiambo <eodhiambo854@gmail.com>
2026-04-11 08:12:30 +0300

    Add intro.py mini-lab

---
+++ SHARED/Python/Labs/Phase_0ne/mini-projects/intro.py
@@ -0,0 +1,17 @@
+# This is a mini-project for the "Introduction to Programming" course.
+# The goal of this project is to practice using print statements to output information about yourself.  
+# Instructions: 
+# 1. Create a print statement that outputs your name
+print("My name is: Intuitive Dialect")
+# 2. Create a print statement that outputs the name of the school you attend
+print("I study at: The University of the Internet")
+# 3. Create a print statement that outputs your major
+print("I study: Electrical Engineering")
+
+# 4. Create a print statement that outputs a sentence that includes all of the above information
+print("Hello, my name is Intuitive Dialect and I study Electrical Engineering at the University of Internet.")
+# 5. Create a print statement that outputs the following pattern:
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
