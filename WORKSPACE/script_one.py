caacbde271392a6a813da5759f4c7dd5d1bdf6ee
Ouma Emmanuel Odhiambo <eodhiambo854@gmail.com>
2026-04-11 08:15:47 +0300

    add script_one.py to workspace folder

---
+++ WORKSPACE/script_one.py
@@ -0,0 +1,9 @@
+print("My name is", "Intuitive Dialect")
+# Variables can be used to store information and make it easier to reuse that information later in the code.
+name = "Intuitive Dialect"
+school = "University of Internet"
+major = " Electrical Engineering"
+print("My name:", name)
+print("I study at:", school)
+
+# Data types are the different kinds of values that can be stored in variables. Common data types include strings (text), integers (whole numbers), floats (decimal numbers), and booleans (true/false values).
