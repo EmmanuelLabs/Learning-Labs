460c9b2e456b7e1a5ecd3d0e018271c1e50f6e16
Ouma Emmanuel Odhiambo <eodhiambo854@gmail.com>
2026-04-19 08:59:54 +0300

    Add types.py roughwork

---
+++ WORKSPACE/types.py
@@ -0,0 +1,16 @@
+frequency = 900
+print(type(frequency))
+
+power = 23.5
+print(type(power))
+
+name = "Router-01"
+print(type(name))
+
+active = True
+print(type(active))
+
+power = str(power)
+print(type(power))
+
+print(frequency, power, name, active)
